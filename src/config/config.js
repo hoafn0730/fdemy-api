82b7dd6afe535c5c540be1f58b769e030dbec0b9
hoafn0730 <hoantran0730@gmail.com>
2024-10-06 21:41:16 +0700

    update production

--- src/config/config.js
+++ src/config/config.js
@@ -19,10 +19,13 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
-        dialect: 'mysql',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_CONNECTION,
+        logging: false,
+        timezone: '+07:00',
     },
 };
